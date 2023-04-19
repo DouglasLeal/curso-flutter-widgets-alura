df31dcc24bb51bf76dfadc2af4ba656c9700d157
Douglas Leal <douglas.leal@live.com>
2023-04-19 19:46:40 -0300

    InitialScreen modificado

--- lib/screens/initial_screen.dart
+++ lib/screens/initial_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets/components/task.dart';
 import 'package:flutter_widgets/data/task_inherited.dart';
 import 'package:flutter_widgets/screens/form_screen.dart';
+
+import '../data/task_dao.dart';
 
 class InitialScreen extends StatefulWidget {
   const InitialScreen({Key? key}) : super(key: key);
@@ -15,20 +18,94 @@ class _InitialScreenState extends State<InitialScreen> {
     return Scaffold(
       appBar: AppBar(
         leading: Container(),
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {});
+            },
+            icon: const Icon(Icons.refresh),
+          ),
+        ],
         title: const Text('Tarefas'),
       ),
-      body: ListView(
-        padding: const EdgeInsets.only(top: 8,bottom: 70),
-        children: TaskInherited.of(context).taskList,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 8, bottom: 70),
+        child: FutureBuilder<List<Task>>(
+            future: TaskDao().findAll(),
+            builder: (context, snapshot) {
+              List<Task>? items = snapshot.data;
+              switch (snapshot.connectionState) {
+                case ConnectionState.none:
+                  return Center(
+                    child: Column(
+                      children: const [
+                        CircularProgressIndicator(),
+                        Text('Carregando'),
+                      ],
+                    ),
+                  );
+
+                case ConnectionState.waiting:
+                  return Center(
+                    child: Column(
+                      children: const [
+                        CircularProgressIndicator(),
+                        Text('Carregando'),
+                      ],
+                    ),
+                  );
+                case ConnectionState.active:
+                  return Center(
+                    child: Column(
+                      children: const [
+                        CircularProgressIndicator(),
+                        Text('Carregando'),
+                      ],
+                    ),
+                  );
+                case ConnectionState.done:
+                  if (snapshot.hasData && items != null) {
+                    if (items.isNotEmpty) {
+                      return ListView.builder(
+                          itemCount: items.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final Task tarefa = items[index];
+                            return tarefa;
+                          });
+                    }
+                    return Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: const [
+                            Icon(
+                              Icons.error_outline,
+                              size: 128,
+                            ),
+                            Text(
+                              'Não há nenhuma Tarefa',
+                              style: TextStyle(fontSize: 32),
+                            ),
+                          ],
+                        ));
+                  }
+                  return const Text('Erro ao carregar tarefas');
+              }
+              return const Text('Erro desconhecido');
+            }),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
             context,
             MaterialPageRoute(
-              builder: (contextNew) => FormScreen(taskContext: context,),
+              builder: (contextNew) => FormScreen(
+                taskContext: context,
+              ),
             ),
-          );
+          ).then((value) => setState(() {
+            print('Recarregando a tela inicial');
+          }));
         },
         child: const Icon(Icons.add),
       ),
